7db2adc396660718b86fb600077522a16b00f5f1
nightslasher758 <82421776+nightslasher758@users.noreply.github.com>
2021-04-16 18:45:38 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+a = "Hi I Am CortanaEX Answer the following questions Enjoy " 
+print(a)
+b = input("1.What is 10 * 10 =")
+
+c = input("2.What is 9 + 1=")
+if c == "10":
+  print("Good Job")
+else:
+  print("Incorrect")
+
+d = input("3.What is 8 / 4=")
+if d == "2":
+  print("Cool")
+else:
+  print("Incorrect")
+
+k = "Great You Master The Levels"
+print(k)
+
